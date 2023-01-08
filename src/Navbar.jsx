ce1c9da82f4e6747732711a62a4e10b0cf29a069
a2zarslaan <a2zarslaan@hotmail.com>
2023-01-08 18:27:28 +0000

    Navlink for nav

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,11 @@
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 export default function Navbar() {
+	let activeStyle = {
+		backgroundColor: 'white',
+		color: 'black',
+		fontWeight: 'bold',
+	};
 	return (
 		<>
 			<div className=' md:px-10 py-2 md:py-5 flex flex-col lg:flex-row items-center justify-between font-normal h-[10%] w-100'>
@@ -9,34 +14,40 @@ export default function Navbar() {
 						to='/'
 						className='font-extrabold justify-center text-xl xl:text-2xl 2xl:text-4xl'
 					>
-						DigiShelf
+						<span className='hover:font-extrabold hover:shadow-lg hover:shadow-white'>
+							DigiShelf
+						</span>
 					</Link>
 				</div>
 				<div className='flex gap-4 xl:gap-12 justify-between xl:text-lg 2xl:text-2xl'>
-					<Link
+					<NavLink
 						to='/'
 						className='px-2 py-1 lg:w-20 2xl:w-28 flex justify-center hover:text-black hover:bg-white hover:font-bold'
+						// style={({ isActive }) => (isActive ? activeStyle : undefined)}
 					>
 						Home
-					</Link>
-					<Link
+					</NavLink>
+					<NavLink
 						to='/about'
 						className='px-2 py-1 lg:w-20 2xl:w-28 flex justify-center hover:text-black hover:bg-white hover:font-bold'
+						style={({ isActive }) => (isActive ? activeStyle : undefined)}
 					>
 						About
-					</Link>
-					<Link
+					</NavLink>
+					<NavLink
 						to='/projects'
 						className='px-2 py-1 lg:w-20 2xl:w-28 flex justify-center hover:text-black hover:bg-white hover:font-bold'
+						style={({ isActive }) => (isActive ? activeStyle : undefined)}
 					>
 						Projects
-					</Link>
-					<Link
+					</NavLink>
+					<NavLink
 						to='/contact'
 						className='px-2 py-1 lg:w-20 2xl:w-28 flex justify-center hover:text-black hover:bg-white hover:font-bold'
+						style={({ isActive }) => (isActive ? activeStyle : undefined)}
 					>
 						Contact
-					</Link>
+					</NavLink>
 				</div>
 			</div>
 		</>
